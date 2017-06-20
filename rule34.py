6b813e32c0fa07fe56f128e085a6e0896e112c9a
Neyot <almorgan9@gmail.com>
2017-06-20 18:43:49 +0000

    Changelog in README.md

--- rule34.py
+++ rule34.py
@@ -4,130 +4,237 @@ from html import escape
 from time import gmtime, strftime, time
 from datetime import datetime, timedelta
 
-logging.basicConfig(level=logging.INFO)
-waitingToSend = False
-xmlfile = "/var/www/html/botdata.xml"
+# ##################### #
+#                       #
+# Setting up debugging. #
+#                       #
+# ##################### #
+
+debugMode = True
+if debugMode:
+    logging.basicConfig(level=logging.INFO) # Setup logging to the console.
+
+def debug(debugMsg):
+    if debugMode:
+        print("[Debug] " + debugMsg)
+
+# ######################################################################################## #
+#                                                                                          #
+# Initial commands to setup things such as logging, Reddit praw, discord and the XML file. #
+#                                                                                          #
+# ######################################################################################## #
+
+waitingToSend = False # Making a global boolean to be used in send_random_message and choose_random_message.
+xmlfile = "/var/www/html/botdata.xml" # The location of the XML file to log data to.
 r = praw.Reddit(client_id="your client id",client_secret="your client secret",user_agent="your user agent")
-client = discord.Client()
-tree = etree.parse(xmlfile)
-root = tree.getroot()
+client = discord.Client() # Creating the Discord Client object (the bot)
+tree = etree.parse(xmlfile) # Parsing the XML into a variable
+root = tree.getroot() # Parsing the root of the XML into a variable.
+
+# ###################################################### #
+#                                                        #
+# Check if the log file exists. If it doesn't, make one. #
+#                                                        #
+# ###################################################### #
 
 try:
     f = open("/var/log/discordBots/redditBot.log","r")
-    f.close()
 except IOError:
     f = open("/var/log/discordBots/redditBot.log","w")
+finally:
+    f.close()
+
+# ############################################################ #
+#                                                              #
+# Sending a random message after a user has entered a command. #
+#                                                              #
+# ############################################################ #
 
 @asyncio.coroutine
-def send_random_message(chnl):
+def send_random_message(channel): # This function is triggered whenever someone uses the command !reddit or !rule34.
     global waitingToSend
-    if not waitingToSend:  #TODO: Remove scheduler and add asyncio.sleep instead
-        currentTime = datetime.now()
-        nextHalfHour = random.randrange(30) + 1
-        plannedTime = currentTime + timedelta(minutes=nextHalfHour)
+    if not waitingToSend: # However, if the function has already been run within the past 30 minutes (or less), it will not run again.
         waitingToSend = True
+        currentTime = datetime.now()
+        nextHalfHour = random.randrange(30) + 1 # Choose a random number between 1 and 30
+        plannedTime = currentTime + timedelta(minutes=nextHalfHour) # Get the date to print to the console.
         print("[+] Job 'choose_random_message' set to execute at " + plannedTime.strftime('%d/%m/%Y %H:%M:%S') + " (in " + str(nextHalfHour) + " minutes).")
-        yield from asyncio.sleep(nextHalfHour*60)
-        yield from choose_random_message(chnl)
+        yield from asyncio.sleep(nextHalfHour*60) # Wait for the amount of minutes in nextHalfHour
+        yield from choose_random_message(channel) # Execute choose_random_message
 
 @asyncio.coroutine
 def choose_random_message(channel):
     global waitingToSend
-    waitingToSend = False
-    x = random.randrange(1)
+    waitingToSend = False # Make it so send_random_message can be executed again.
+    x = random.randrange(3) # Choose a number between 0 and the number before the number in the brackets.
     print("[+] Time has been reached. Sending message ID: " + str(x))
-    print(channel.name, channel.server.name)
-    if x == 0:
+    if x == 0: # Just say "I love the water"
         yield from client.send_message(channel, "I love the water.")
+    elif x == 1: # Choose a random person in the server and say you hate them.
+        i = 0
+        randomUserInServer = random.randrange(channel.server.member_count)
+        for y in channel.server.members:
+            if i == randomUserInServer:
+                yield from client.send_message(channel, "I don't like " + y.mention + " anymore. Please, just fuck off would you?")
+                break
+            else:
+                i += 1
+    elif x == 2: # Make the bot perform !r34 to itself.
+        yield from client.send_message(channel, "!r34")
+        post = r.subreddit('rule34').random()
+        yield from asyncio.sleep(0.5)
+        add_count(client.user, "rule34", True)
+        yield from client.send_message(channel, post.url)
+        
 
-def update_user(usrObj):
+# ################################ #
+#                                  #
+# Working with XML (logging data). #
+#                                  #
+# ################################ #
+
+def update_user(usrObj): # When a user joins or updates their name, update their name in the XML file. If they don't exist, make an element for them.
     for user in root.findall('user'):
         if user.attrib["id"] == str(usrObj.id):
             user.attrib["name"] = escape(usrObj.name)
             tree.write(xmlfile)
             return
     newUser = etree.SubElement(root, "user", {"name": escape(usrObj.name), "id": usrObj.id, "dateAdded": str(int(time())), "lastUsed": str(int(time()))}) # if a user doesn't exist, add them to the file
-    tree.write(xmlfile)
+    tree.write(xmlfile) # Write to the XML file.
 
-def add_count(usrObj, subName, subExists):
-    subName = escape(subName)
+def add_count(usrObj, subName, subExists): # Increase the value of a sub for a user.
+    subName = escape(subName) # Escape the subreddit's name.
     for user in root.findall('user'):
         if user.attrib["id"] == str(usrObj.id):
             currentTime = str(int(time()))
-            user.attrib["lastUsed"] = currentTime
+            user.attrib["lastUsed"] = currentTime # Make the lastUsed attribute the current time in epoch.
             if check_element_exists(usrObj.id, subName):
                 for sub in user:
                     if sub.attrib["name"] == subName:
-                        sub.attrib["val"] = str(int(sub.attrib["val"]) + 1)
+                        sub.attrib["val"] = str(int(sub.attrib["val"]) + 1) # Increase the value of the sub by one.
                         sub.attrib["last"] = currentTime
                         tree.write(xmlfile)
                         return
             else:
-                subType = "sub" if subExists else "failedSub"
+                subType = "sub" if subExists else "failedSub" # If the sub exists, make the element a sub, if it doesn't, make it a failedSub
                 newSub = etree.SubElement(user, subType, {"name": subName, "last": currentTime, "val": "1"})
                 tree.write(xmlfile)
                 return
 
-def check_element_exists(userID, subName):
-    for user in root.findall('user'):
-        if user.attrib["id"] == str(userID):
-            for sub in user:
+def check_element_exists(userID, subName): # Check if a sub has already been used by the user.
+    for user in root.findall('user'): 
+        if user.attrib["id"] == str(userID): # If the user is equal to the author,
+            for sub in user: # loop through subs.
                 if sub.attrib["name"] == subName:
                     return True
-    return False
+    return False # If the loop didn't return True, return False.
+
+# #################################### #
+#                                      #
+# Displaying the XML data to the user. #
+#                                      #
+# #################################### #
+
+@asyncio.coroutine
+def findUserTop10Subs(channel, nameToFind): # Read the XML file and get the subs of the user.
+    usersFound = [] # Make the array to contain all the users matching the search term.
+    for user in root.findall('user'):
+        if nameToFind.lower() in user.attrib["name"].lower():
+            usersFound.append(user) # If the search term is in the users name, add them to the usersFound array.
+    if len(usersFound) == 0: # If the array is empty, no users matched the search terms.
+        print("[-] No users found.")
+        yield from client.send_message(channel, "```No users found.```") # Inform the user.
+    elif len(usersFound) == 1: # If one user was found, find their top 10 subs.
+        top10Subs = [] # Make the array to contain all the subs that were found in the users element.
+        for sub in usersFound[0]:
+            top10Subs.append(sub) # Add all the subs to the array.
+        top10Subs.sort(key=lambda x: int(x.attrib["val"]), reverse=True) # Sort the subs by the amount of times they were used by the user, with the most used being at top10Subs[0].
+        subLeaderboardMsg = "" # A string that will contain the names and the values of the top 10 items in the array.
+        if len(top10Subs) > 0: # If the array is bigger than 0, then add the subs to the string.
+            for sub in top10Subs[:10]: # Loop through only the first 10 items in the array.
+                subLeaderboardMsg += sub.attrib["name"] + " - " + sub.attrib["val"] + "\n"
+        else: # If no items are in the array, the user has not used the bot.
+            subLeaderboardMsg = "No data found for this user.\n" # Inform the user.
+        print("[+] Retrieved stats of user '" + usersFound[0].attrib["name"] + "'.")
+        yield from client.send_message(channel, "User '" + usersFound[0].attrib["name"] + "':\n```" + subLeaderboardMsg + "```") # Show the user the top 10 subs of the name they requested.
+    else: # If multiple users were found, inform the user.
+        userNamesFound = "" # A string that will contain all the names of the users that matched the search terms.
+        for user in usersFound:
+            userNamesFound += user.attrib["name"] + "\n"
+        print("[-] Multiple users found.")
+        yield from client.send_message(channel, "Multiple users found: ```\n" + userNamesFound + "```")
+
+@asyncio.coroutine
+def getTopThreeUsers(channel):
+    pass
+
+# ######################### #
+#                           #
+# Main Discord interaction. #
+#                           #
+# ######################### #
 
 @client.event
-@asyncio.coroutine 
-def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
+@asyncio.coroutine
+def on_message(message): # Event: When the bot sees a message in a channel.
+    if message.author == client.user: # Making sure the bot does not reply to itself.
         return
 
-    if message.content.startswith('!rule34') or message.content.startswith('!r34'):
-        #msg = 'Hello {0.author.mention}'.format(message)
-        #yield from client.send_message(message.channel, msg)
+    if message.content.lower().startswith('!rule34') or message.content.lower().startswith('!r34'):
         sub = r.subreddit('rule34')
-        post = sub.random()
+        post = sub.random() # Get a random post from the sub /r/rule34.
         print("[+] '" + message.author.name + "' asked for a random post on '/r/rule34': " + post.url)
-        with open("/var/log/discordBots/redditBot.log","a") as f:
+        with open("/var/log/discordBots/redditBot.log","a") as f: # Log this event to a file.
             f.write("["+strftime("%Y-%m-%d %H:%M:%S", gmtime())+"] '" + message.author.name + "' asked for a random post on '/r/rule34': " + post.url + "\n")
-        add_count(message.author, "rule34", True)
+        add_count(message.author, "rule34", True) # Add 1 to this subreddit value under this user in the XML file.
         yield from client.send_message(message.channel, post.url)
+        yield from send_random_message(message.channel) # Activate the send_random_message function.
 
-    elif message.content.startswith('!reddit '):
+    elif message.content.lower().startswith('!reddit '):
         subToCheck = message.content.split(' ')[1].lower()
         try:
-            post = r.subreddit(subToCheck).random()
+            post = r.subreddit(subToCheck).random() # Get a random post from the sub /r/rule34.
             print("[+] '" + message.author.name + "' asked for a random post on '/r/" + subToCheck + "': " + post.url)
-            with open("/var/log/discordBots/redditBot.log","a") as f:
+            with open("/var/log/discordBots/redditBot.log","a") as f: # Log this event to a file.
                 f.write("["+strftime("%Y-%m-%d %H:%M:%S", gmtime())+"] '" + message.author.name + "' asked for a random post on '/r/" + subToCheck + "': " + post.url + "\n")
-            add_count(message.author, subToCheck, True)
+            add_count(message.author, subToCheck, True) # Add 1 to this subreddit value under this user in the XML file.
             yield from client.send_message(message.channel, post.url)
-        except Exception as e:
+        except Exception as e: # If the sub doesn't exist (or another error occurs), tell the user.
             add_count(message.author, subToCheck, False)
             fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
             yield from client.send_message(message.channel, fmt.format(type(e).__name__, e))
-        yield from send_random_message(message.channel)
+        yield from send_random_message(message.channel) # Activate the send_random_message function.
 
+    elif message.content.lower().startswith('!stats'):
+        msgArgs = message.content.split(' ')
+        msgArgs.pop(0) # Remove !stats from the array of args.
+        nameToFind = escape(" ".join(msgArgs)) # Convert the name to one with HTML escape codes.
+        if len(msgArgs) > 0: # If the user inputted a name, search for it.
+            print("[+] '" + message.author.name + "' searched for the stats of '" + nameToFind + "'.")
+            yield from findUserTop10Subs(message.channel, nameToFind)
+        else: # Else, get the top three users of !r34.
+            yield from getTopThreeUsers(message.channel)
 
-    #elif message.content.startswith('!'):
-    #    yield from client.send_message(message.channel, "Are you trying to bait me you fucking faggot? Do that again and you will truly burn in hell.")
+    elif message.content.lower().startswith('!gay'):
+        print("[+] '" + message.author.name + "' asked for help.")
+        yield from client.send_message(message.channel, "```\nCommands -\n\n!r34 / !rule34 - a shortcut for '!reddit rule34'. Also the main feature of this bot.\n!reddit <sub> - Sends a random post from a subreddit to the channel.\n!stats <user> - Shows the statistics for that user.\n!stats - Shows the top three users to use !r34.\n!gay - Access this help.\n\nPlaying !gay reference: https://youtu.be/hUdp1QEcxiY?t=1m40s" + "\n```")
 
 @client.event
 @asyncio.coroutine 
-def on_ready():
-    print('Logged in as ' + client.user.name)
+def on_ready(): # Event: When the bot successfully joined the servers.
+    print('Logged in as '+client.user.name+' (ID: '+client.user.id+') | '+str(len(client.servers))+' servers') # Print the bot's username, Discord ID and the amount of servers it's connected to.
     print('--------------')
-    yield from client.change_presence(game=discord.Game(name='Gay'))
+    yield from client.change_presence(game=discord.Game(name='!gay')) # On Discord, this displays under the bot as "Playing !gay".
 
 @client.event
 @asyncio.coroutine
-def on_member_join(member):
+def on_member_join(member): # Event: When a member joins a server the bot's connected to.
     update_user(member)
 
 @client.event
 @asyncio.coroutine
-def on_member_update(old, member):
+def on_member_update(old, member): # Event: When a member updates their nickname.
     update_user(member)
 
+print("Starting...") # Start up the bot.
 client.run('your discords bot token')
